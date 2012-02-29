0ed9dcc201b94347818f695bf94a808abda42dd8
Matthias Hogerheijde <matthias@hogerheijde.net>
2012-02-29 15:43:55 +0100

    Modify buildscript to include versionnumber

--- build.sh
+++ build.sh
@@ -38,6 +38,8 @@ ROOT_FILES=        # put these files in root of xpi (space separated list of lea
 ROOT_DIRS=         # ...and these directories       (space separated list)
 BEFORE_BUILD=      # run this before building       (bash command)
 AFTER_BUILD=       # ...and this after the build    (bash command)
+SOURCE_DIR=`pwd`/src
+BUILD_DIR=`pwd`/build
 
 #uncomment to debug
 #set -x
@@ -54,26 +56,35 @@ if [ -z $APP_NAME ]; then
   exit;
 fi
 
-SOURCE_DIR=`pwd`/src
 ROOT_DIR=`pwd`
-BUILD_DIR=`pwd`/build
 
-
-# remove any left-over files from previous build
-rm -f $APP_NAME.xpi
-rm -rf $BUILD_DIR
 
 # Running commands before building
 $BEFORE_BUILD
 
-mkdir --parents --verbose $BUILD_DIR
+if [ ! -d "$BUILD_DIR" ]; then
+	mkdir --parents --verbose $BUILD_DIR
+fi
 
-# generate the XPI file
-echo "Generating $APP_NAME.xpi..."
+# Change into source directory, that is the point of view we want.
 cd $SOURCE_DIR
 
-zip -r $BUILD_DIR/$APP_NAME.xpi *
+# Fetch the versionnumber from install.rdf
+VERSION_NUMBER=`grep "<em:version>.*</em:version>" install.rdf | cut -f2 -d ">" | cut -f1 -d "<"`
 
+APP_FILE=$BUILD_DIR/$APP_NAME-$VERSION_NUMBER.xpi
+
+if [ -z $APP_FILE ]; then
+	echo "Removing $APP_FILE..."
+	rm $APP_FILE
+fi
+# generate the XPI file
+echo "Generating $APP_FILE..."
+
+# Zip all files in $SOURCE_DIR into the .xpi
+zip -r $APP_FILE *
+
+# Back to start
 cd "$ROOT_DIR"
 
 # Runing commads after building
